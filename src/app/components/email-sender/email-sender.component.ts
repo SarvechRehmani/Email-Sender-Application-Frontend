51c1727eb362ad0e661ab433807296d8e600881d
Sarvech <sarwech99@gmail.com>
2024-09-02 19:04:01 +0500

    implemented rich text editor.

--- src/app/components/email-sender/email-sender.component.ts
+++ src/app/components/email-sender/email-sender.component.ts
@@ -6,11 +6,13 @@ import { ToastrService } from 'ngx-toastr';
 import { SendMailService } from '../../service/send-mail.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MailRequest } from '../../models/MailRequest';
+import { EditorComponent } from '@tinymce/tinymce-angular';
+import { API_KEY_TINYMCE } from '../../service/helper';
 
 @Component({
   selector: 'app-email-sender',
   standalone: true,
-  imports: [FormsModule, CommonModule, HttpClientModule],
+  imports: [FormsModule, CommonModule, HttpClientModule, EditorComponent],
   providers: [SendMailService], // Optional, if not using providedIn: 'root'
   templateUrl: './email-sender.component.html',
   styleUrls: ['./email-sender.component.css'],
@@ -20,6 +22,60 @@ export class EmailSenderComponent {
     private toast: ToastrService,
     private sendMailService: SendMailService
   ) {}
+
+  api_key = API_KEY_TINYMCE;
+  init: EditorComponent['init'] = {
+    plugins: [
+      // Core editing features
+      'anchor',
+      'autolink',
+      'charmap',
+      'codesample',
+      'emoticons',
+      'image',
+      'link',
+      'lists',
+      'media',
+      'searchreplace',
+      'table',
+      'visualblocks',
+      'wordcount',
+      // Your account includes a free trial of TinyMCE premium features
+      // Try the most popular premium features until Sep 16, 2024:
+      'checklist',
+      'mediaembed',
+      'casechange',
+      'export',
+      'formatpainter',
+      'pageembed',
+      'a11ychecker',
+      'tinymcespellchecker',
+      'permanentpen',
+      'powerpaste',
+      'advtable',
+      'advcode',
+      'editimage',
+      'advtemplate',
+      'mentions',
+      'tinycomments',
+      'tableofcontents',
+      'footnotes',
+      'mergetags',
+      'autocorrect',
+      'typography',
+      'inlinecss',
+      'markdown',
+    ],
+    toolbar:
+      'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ],
+  };
+
   email: MailRequest = {
     to: [''],
     cc: [''],
--- src/app/service/helper.ts
+++ src/app/service/helper.ts
@@ -1,1 +1,3 @@
 export const API_URL = 'http://localhost:8080/api/v1/email';
+export const API_KEY_TINYMCE =
+  'w5897ekr1xyrerqaxgrp3acmuunkn8rxot0oqnolkbdf6wgk';
